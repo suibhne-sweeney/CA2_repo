2d561e448c81f26dbde8639c52e42e3ccbfc0679
suibhne-sweeney <suibhnesweeney@gmail.com>
2022-12-11 14:55:58 +0000

    spring cleaning

--- backend/Travel_Agency/shop/context_processors.py
+++
@@ -1,5 +0,0 @@
-from .models import Category
-
-def menu_links(requset):
-    links = Category.objects.all()
-    return {"links": links}
--- backend/Travel_Agency/shop/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'shop'
-urlpatterns = [
-    path('', views.prod_list, name = 'all_products'),
-    path('<uuid:category_id>/', views.prod_list, name = 'products_by_category'),
-    path('<uuid:category_id>/<uuid:product_id>/', views.product_detail, name = 'products_detail'),
-]
